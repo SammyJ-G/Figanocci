cbd2b451ebe1df6a1a9eeeb49ee65d20ccba354d
samjenkins <shinobislayer12@gmail.com>
2023-04-27 10:30:53 -0400

    slatt

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Let's run FIbbonacci");
+        FIbonacciLike Fibonacci = new FIbonacciLike(5, 2, 4);
+        // 2 4 6 10 16
+    Fibonacci.run();
+
+        FIbonacciLike Fibonacci2 = new FIbonacciLike(7, -1, 2);
+        // -1 2 1 3 4 7 11
+        Fibonacci2.run();
+
+
     }
 }
